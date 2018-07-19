c72f50b2a860391897f49a0cb061d400efba54c1
Andrea <andreazepeda003@gmail.com>
2018-07-19 18:57:25 -0500

    uncommented

--- server.js
+++ server.js
@@ -59,11 +59,11 @@ app.use(router);
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/mongoHeadlines";
 
-// // Connect to the Mongo DB 
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+// Connect to the Mongo DB 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 
 // Start the server
