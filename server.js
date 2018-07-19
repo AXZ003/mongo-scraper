2e746aaccfdd0bd98d0f04883db4fb721be0595c
Andrea <andreazepeda003@gmail.com>
2018-07-19 18:54:04 -0500

    changed app.engine

--- server.js
+++ server.js
@@ -38,11 +38,15 @@ app.use(express.static("public"));
 
 
 // handlebars
-app.engine("handlebars", exphbs({
-    defaultLayout: "main",
-    partialsDir: path.join(__dirname, "/views/layouts")
-}));
+// app.engine("handlebars", exphbs({
+//     defaultLayout: "main",
+//     partialsDir: path.join(__dirname, "/views/layouts")
+// }));
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 
 var router = express.Router();
 
@@ -55,11 +59,11 @@ app.use(router);
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/mongoHeadlines";
 
-// Connect to the Mongo DB 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+// // Connect to the Mongo DB 
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 
 // Start the server
