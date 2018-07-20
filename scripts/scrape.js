9a209e0cdb262d1361ac1d158683f62830121674
Andrea <andreazepeda003@gmail.com>
2018-07-19 19:57:49 -0500

    added scripts

---
+++ scripts/scrape.js
@@ -0,0 +1,27 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
+var scrape = function(callback) {
+
+   var dbArticle = [];
+
+   request("https://www.huffingtonpost.com/section/travel", function(error, response, html) {
+
+     var $ = cheerio.load(html);
+
+     $('.card_contentg').each(function(i, element) {
+
+       var result = {};
+
+       result.title = $(this).children("a").text();
+       result.link = $(this).children("a").attr("href");
+
+       if (result.title !== "" && result.link !== "") {
+           dbArticle.push(result);
+       }
+     });
+     callback(dbArticle);
+   });
+};
+
+module.exports = scrape;
