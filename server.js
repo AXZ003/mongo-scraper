ceddeec2cbf7713f300145048b6157c227d95202
Andrea <andreazepeda003@gmail.com>
2018-07-19 20:46:28 -0500

    promise

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(router);
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/mongoHeadlines";
 
 // // Connect to the Mongo DB 
-// mongoose.Promise = Promise;
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 
